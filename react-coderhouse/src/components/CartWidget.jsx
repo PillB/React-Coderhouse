c880b2ffed9c86884ec5a20738e1f324a50ecac7
PillB <11358989+PillB@users.noreply.github.com>
2024-03-14 19:03:27 -0500

    Create CartWidget

---
+++ react-coderhouse/src/components/CartWidget.jsx
@@ -0,0 +1,13 @@
+// CartWidget code
+import React from 'react';
+
+const CartWidget = () => {
+  return (
+    <div>
+      <img src=""/>{/* CartWidget icon */}
+      <span>3</span> {/* Hardcoded item count */}
+    </div>
+  );
+};
+
+export default CartWidget;
