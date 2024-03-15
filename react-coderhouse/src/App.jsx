fae808564bf657e629c9f60ec98ccabe78b0d99f
PillB <11358989+PillB@users.noreply.github.com>
2024-03-14 19:20:57 -0500

    Update App.jsx

--- react-coderhouse/src/App.jsx
+++ react-coderhouse/src/App.jsx
@@ -1,7 +1,7 @@
 // src/App.js
 import React from 'react';
-import NavBar from '/components/NavBar';
-import ItemListContainer from '/ItemListContainer';
+import NavBar from './components/Navbar';
+import ItemListContainer from './ItemListContainer';
 
 const App = () => {
   return (
