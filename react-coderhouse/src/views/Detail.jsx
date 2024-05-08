a81c02aa1337624a84d2e1c6602fbcbc389e07a5
PillB <11358989+PillB@users.noreply.github.com>
2024-05-07 23:51:23 -0500

    Update Detail.jsx
    Fixed total amount and details from order

--- react-coderhouse/src/views/Detail.jsx
+++ react-coderhouse/src/views/Detail.jsx
@@ -1,15 +1,17 @@
 // src/views/Detail.jsx
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { useParams } from 'react-router-dom';
 import { db } from '../FirebaseConfig';
 import { doc, getDoc } from "firebase/firestore";
 import { Container, ListGroup, ListGroupItem } from 'react-bootstrap';
+import { CartContext } from '../context/cartContext';
 
 export const Detail = () => {
     const { id: orderId } = useParams();  // Correct parameter extraction
     const [order, setOrder] = useState(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState('');
+    const { cartItems, total, clearCart } = useContext(CartContext);
 
     useEffect(() => {
         if (orderId) {
@@ -19,6 +21,7 @@ export const Detail = () => {
                     const docSnap = await getDoc(orderDoc);
                     if (docSnap.exists()) {
                         setOrder(docSnap.data());
+                        console.log("Last order",docSnap.data());
                     } else {
                         console.log("No such document!");
                         setError("No order found with the provided ID.");
@@ -54,11 +57,16 @@ export const Detail = () => {
                 <ListGroupItem>Address: {order?.buyer.address}</ListGroupItem>
                 <ListGroupItem>Total: ${order?.total}</ListGroupItem>
                 <ListGroupItem>Date: {order?.date}</ListGroupItem>
+                <ListGroup className="mb-3">
                 {order?.items.map((item, index) => (
-                    <ListGroupItem key={index}>
-                        {item.title} - Quantity: {item.quantity} - Price: ${item.price}
-                    </ListGroupItem>
+                    <ListGroup.Item key={index}>
+                       {item.title} - Quantity: {item.quantity} - Price: ${item.price.toFixed(2)} each - Total: ${(item.quantity * item.price).toFixed(2)}
+                    </ListGroup.Item>
                 ))}
+                <ListGroup.Item>
+                    Total: ${order?.total.toFixed(2)}
+                </ListGroup.Item>
+            </ListGroup>
             </ListGroup>
         </Container>
     );
